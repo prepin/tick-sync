7912709cbddacb20a241506b3d84efcea5979926
Paul Repin <paul.repin@gmail.com>
2026-06-18 21:05:31 +0300

    fix failing tests

--- internal/infra/sqlite/migrate/migrate.go
+++ internal/infra/sqlite/migrate/migrate.go
@@ -6,6 +6,7 @@ import (
 	"database/sql"
 	"embed"
 	"fmt"
+	"io/fs"
 
 	"github.com/pressly/goose/v3"
 )
@@ -15,13 +16,17 @@ var migrationsFS embed.FS
 
 // Up applies all pending migrations to the given database.
 func Up(ctx context.Context, db *sql.DB) error {
-	goose.SetBaseFS(migrationsFS)
-
-	if err := goose.SetDialect("sqlite3"); err != nil {
-		return fmt.Errorf("set goose dialect: %w", err)
+	migrations, err := fs.Sub(migrationsFS, "migrations")
+	if err != nil {
+		return fmt.Errorf("load migrations filesystem: %w", err)
 	}
 
-	if err := goose.UpContext(ctx, db, "migrations"); err != nil {
+	provider, err := goose.NewProvider(goose.DialectSQLite3, db, migrations)
+	if err != nil {
+		return fmt.Errorf("create migration provider: %w", err)
+	}
+
+	if _, err := provider.Up(ctx); err != nil {
 		return fmt.Errorf("apply migrations: %w", err)
 	}
 
